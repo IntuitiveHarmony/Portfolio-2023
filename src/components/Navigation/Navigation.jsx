1451bd3c14514bf198c02f301da5e254dbf0329a
Intuitive Harmony <j.horst77@gmail.com>
2023-05-07 22:56:06 -0600

    menu icon

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,8 +1,22 @@
 import "./Navigation.css";
-import { AppBar, Container, Typography } from "@mui/material";
+import { useState } from "react";
+import {
+  AppBar,
+  Container,
+  Typography,
+  Box,
+  Menu,
+  IconButton,
+} from "@mui/material";
 
 function Navigation() {
+  const [collapseMenu, setCollapseMenu] = useState(true);
   const backgroundColor = "rgba(255, 0, 255, .75)";
+  const linkColor = "cyan";
+
+  function handleCollapseMenu() {
+    setCollapseMenu(!collapseMenu);
+  }
 
   return (
     <AppBar
@@ -20,6 +34,8 @@ function Navigation() {
         sx={{
           display: "flex",
           flexDirection: "row",
+          alignItems: "center",
+          flexGrow: 1,
         }}
       >
         <img className="logo" src={process.env.PUBLIC_URL + "/favicon.ico"} />
@@ -29,7 +45,7 @@ function Navigation() {
           component="a"
           href="/Portfolio-2023"
           sx={{
-            paddingLeft: ".5em",
+            paddingLeft: ".3em",
             mr: 2,
             display: { md: "flex" },
             fontFamily: "monospace",
@@ -41,8 +57,36 @@ function Navigation() {
         >
           Jason Horst
         </Typography>
+        <Box
+          sx={{
+            flexGrow: 0,
+            display: { xs: "flex", md: "none" },
+            // justifyContent: "flex-end",
+            alignItems: "center",
+            alignSelf: "flex-end",
+          }}
+        >
+          <IconButton
+            size="large"
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            // onClick={handleOpenNavMenu}
+            sx={{ color: linkColor }}
+          >
+            {collapseMenu ? (
+              <i
+                className="fa-solid fa-bars menu-icon"
+                onClick={() => handleCollapseMenu()}
+              ></i>
+            ) : (
+              <i class="fa-solid fa-x" onClick={() => handleCollapseMenu()}></i>
+            )}
+          </IconButton>
+        </Box>
       </Container>
     </AppBar>
   );
 }
+
 export default Navigation;
